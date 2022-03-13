18f51bfc462f6f9b313171170a5e920261c18653
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2022-03-13 09:12:05 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # install puppet-lint
-package { 'puppet-lint':
-    ensure  => '2.5.0',
-    name    => 'puppet-lint'
+exec { 'apt-get install -y ruby':
+    path => '/usr/bin'
+}
+exec { 'gem install puppet-lint -v 2.5.0':
+    path => '/usr/bin'
 }
