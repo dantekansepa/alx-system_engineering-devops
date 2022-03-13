2778f69effe308a321ea815a9ed50f9ba168c5e2
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2022-03-13 09:35:25 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,6 @@
 exec { 'echo -e "\tPasswordAuthentication no" >> /etc/ssh/ssh_config':
     path => '/bin'
 }
-exec { 'echo -e "\tIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config':
+exec { 'echo -e "\tIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
     path => '/bin'
 }
