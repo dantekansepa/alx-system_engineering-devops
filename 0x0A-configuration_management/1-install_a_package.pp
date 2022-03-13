284632c82787f998a5b854ddd24b5e6f35a5e4d9
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2022-03-13 08:59:53 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-# install puppet-lint
-package { 'puppet-lint':
-    ensure  => '2.5.0',
-    name    => 'puppet-lint',
-}
+ ​#​ Installs a package with: 
+ ​#​        name: puppet-lint 
+ ​#​        version: 2.5.0 
+  
+ ​package​ {​'puppet-lint'​: 
+ ​ensure   ​=> ​'​2.5.0​'​, 
+ ​provider ​=> ​'​gem​'​, 
+ ​}
