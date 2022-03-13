a2dc169b60f199df1d9193bec0f6b10884729629
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2022-03-13 08:39:05 +0200

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kills a process named killmenow
-exec { 'killmenow':
+exec { 'pkill -f killmenow':
     path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
     command => 'pkill "killmenow"'
 }
